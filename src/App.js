756517e650dc76c6566b6a568691d8b54b0cdae3
GideonOdiokine <gidysmartodiokineh>
2021-06-04 03:04:30 +0100

    added localstorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import TodoApp from "./components/TodoApp";
 // import SimpleForm from "./components/SimpleForm";
-import SwMovies from "./components/SwMovies";
 
 
 function App() {
  return(
    <div>
      <TodoApp/>
-     <SwMovies/>
+
    </div>
  )
 }
