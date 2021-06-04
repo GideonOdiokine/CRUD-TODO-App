d7e1ddf2b30ee3907a353dcc3ef7b717f00c9cd4
GideonOdiokine <gidysmartodiokineh>
2021-06-04 02:07:01 +0100

    added toggler to component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,31 +1,27 @@
 import React from "react";
-import Paper from "@material-ui/core/Paper";
 import TextField from "@material-ui/core/TextField";
-import useStateInput from "../hooks/useStateInput";
-
+import Paper from "@material-ui/core/Paper";
+import useInputState from "../hooks/useStateInput";
 function TodoForm({ addTodo }) {
-  const [value, handleChange, reset] = useStateInput("");
+  const [value, handleChange, reset] = useInputState("");
   return (
-    <Paper
-      style={{
-        position: "relative",
-        top: "60px",
-        bottom: "10px",
-        margin: "1rem 0",
-        padding: "0 1rem",
-      }}
-    >
+    <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
-        onSubmit={(e) => {
+        onSubmit={e => {
           e.preventDefault();
           addTodo(value);
           reset();
         }}
       >
-        <TextField value={value} onChange={handleChange} fullWidth />
+        <TextField
+          value={value}
+          onChange={handleChange}
+          margin='normal'
+          label='Add New Todo'
+          fullWidth
+        />
       </form>
     </Paper>
   );
 }
-
 export default TodoForm;
