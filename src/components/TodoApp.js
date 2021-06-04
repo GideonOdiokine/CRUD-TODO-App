b76a0c845e00cbadc41a0d326d88c2e2469b3b89
GideonOdiokine <gidysmartodiokineh>
2021-06-04 01:41:01 +0100

    added EditTodoForm component

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -30,18 +30,34 @@ function TodoApp(){
         );
         setTodos(updatedTodos);
     }
-    return(
-        <Paper style={{padding:0,margin:0,height:"100vh", backgroundColor:'#fafafa'}} elevation={0} >
-            <AppBar>
-                <Toolbar color='primary' position='static' style={{height:'64px'}}>
-                    <Typography color='inherit'>TODOS WITH HOOKS</Typography>
-                </Toolbar>
-            </AppBar>
-            <TodoForm addTodo={addTodo} />
-            <TodoList toggleTodo={toggleTodo} todos={todos} removeTodo={removeTodo} />
+    return (
+        <Paper
+          style={{
+            padding: 0,
+            margin: 0,
+            height: "100vh",
+            backgroundColor: "#fafafa"
+          }}
+          elevation={0}
+        >
+          <AppBar color='primary' position='static' style={{ height: "64px" }}>
+            <Toolbar>
+              <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+            </Toolbar>
+          </AppBar>
+          <Grid container justify='center' style={{ marginTop: "1rem" }}>
+            <Grid item xs={11} md={8} lg={4}>
+              <TodoForm addTodo={addTodo} />
+              <TodoList
+                todos={todos}
+                removeTodo={removeTodo}
+                toggleTodo={toggleTodo}
+               
+              />
+            </Grid>
+          </Grid>
         </Paper>
-        
-    )
+      );
 }
 
 export default TodoApp;
